f40941f4cea0c66c73c0044d6ef61d8d86f67b4c
test4ment <nkaliczkij@yandex.ru>
2023-09-09 18:54:32 +0600

    added photon torpedo

--- Spacewar/GameLib.cs
+++ Spacewar/GameLib.cs
@@ -43,6 +43,34 @@ public class Spaceship: Object{
     }
 }
 
+public class PhotonTorpedo: Object{
+    Double[]? position;
+    Double[]? instant_velocity;
+
+    public PhotonTorpedo(Double posx, Double posy){
+        this.position = new Double[] {posx, posy};
+    }
+    public void SetPos(Double posx, Double posy){
+        this.position = new Double[] {posx, posy};
+    }
+
+    public void SetVel(Double posx, Double posy){
+        this.instant_velocity = new Double[] {posx, posy};
+    }
+
+    public Double[]? GetPos(){
+        return this.position;
+    }
+
+    public Double[]? GetVel(){
+        return this.instant_velocity;
+    }
+    override public void Move(){
+        this.position[0] += this.instant_velocity[0];
+        this.position[1] += this.instant_velocity[1];
+    }
+}
+
 public class Turret: Object{
     Double[]? position;
     override public void Move(){

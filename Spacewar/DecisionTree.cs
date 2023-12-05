f2e364d413d984889c4413cb3fcf605197385224
test4ment <88853792+test4ment@users.noreply.github.com>
2023-12-05 15:18:35 +0000

    made ITree method virtual

--- Spacewar/DecisionTree.cs
+++ Spacewar/DecisionTree.cs
@@ -1,14 +1,6 @@
 public interface ITree<T, U> where T : notnull where U : notnull {
     public IDictionary<T, U> tree {get;}
-    public virtual void AddRecord(T[] record, U value, IDictionary<T, object> tree){
-        try{
-            tree.TryAdd(record[0], new Dictionary<T, U>());
-            AddRecord(record[1..], value, (IDictionary<T, object>)tree[record[0]]);
-        }
-        catch(IndexOutOfRangeException){
-            tree[record[0]] = value;
-        }
-    }
+    public abstract static void AddRecord(T[] record, U value, IDictionary<T, object> tree);
 }
 
 public class UniversalTree : ITree<object, object>{

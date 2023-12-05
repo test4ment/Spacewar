7bfd36b5e3038e69709d940f9d68ab866d32a453
test4ment <88853792+test4ment@users.noreply.github.com>
2023-12-05 16:13:13 +0000

    deecision tree bdd

--- Spacewar.tests/Tests/DecisionTreeTests.cs
+++ Spacewar.tests/Tests/DecisionTreeTests.cs
@@ -1,17 +1,42 @@
 namespace Spacewar.Tests;
 
-// [FeatureFile(@"../../../Features/decisiontree_collision.feature")]
+[FeatureFile(@"../../../Features/decisiontree_collision.feature")]
 public class DecisionTreeFeatures : Feature{
-    
-    [Fact]
-    public void Test(){
-        var tree = new UniversalTree();
-        object exceptionAction = () => {throw new Exception("Collision!");};
-        
+    public ITree<object, object> tree = new UniversalTree();
+    public readonly object exceptionAction = () => {throw new Exception("Collision!");};
+    public Dictionary<object, object> manual_dict = new Dictionary<object, object>();
+    public Action func = () => {};
+
+    [Given("Заполненное дерево решений")]
+    public void MakeTree(){
         UniversalTree.AddRecord(new object[]{1, 1, -1, -1}, exceptionAction, tree.tree);
+    }
 
+    [When("Сравнение сгенерированного и сделанного вручную дерева")]
+    public void CompareTrees(){
+        var branch4 = new Dictionary<object, object>
+        {
+            { -1, exceptionAction }
+        };
+        var branch3 = new Dictionary<object, object>
+        {
+            { -1, branch4 }
+        };
+        var branch2 = new Dictionary<object, object>
+        {
+            { 1, branch3 }
+        };
+        manual_dict = new Dictionary<object, object>
+        {
+            { 1, branch2 }
+        };
 
-        Assert.Equal(((Dictionary<object, object>)((Dictionary<object, object>)((Dictionary<object, object>)tree.tree[1])[1])[-1])[-1], exceptionAction);
+        func = () => {Assert.Equal(manual_dict, tree.tree);};
+    }
+
+    [Then("Дерево состоит из словаря словарей")]
+    public void AssertDictOfDicts(){
+        func();
     }
 }
 

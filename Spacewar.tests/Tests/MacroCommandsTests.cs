8ce2038410aaf9b68a4e30fc90af7041812ebe12
test4ment <nkaliczkij@yandex.ru>
2023-12-10 01:57:51 +0600

    formatting

--- Spacewar.tests/Tests/MacroCommandsTests.cs
+++ Spacewar.tests/Tests/MacroCommandsTests.cs
@@ -1,16 +1,19 @@
-namespace Spacewar.Tests;
+﻿namespace Spacewar.Tests;
 
 [FeatureFile(@"../../../Features/macrocommand.feature")]
-public class MacroCommandsFeature : Feature{
+public class MacroCommandsFeature : Feature
+{
     public Mock<UObject> test_obj = new Mock<UObject>(new ObjDictionary());
 
     [Given("Инициализирован IoC")]
-    public static void IoCInit(){
+    public static void IoCInit()
+    {
         new InitScopeBasedIoCImplementationCommand().Execute();
     }
 
     [And("Сгенерированы разные команды")]
-    public void MakeCommands(){
+    public static void MakeCommands()
+    {
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register",
             "Game.StartCommand",
             (object[] args) => new StartCommand((Order)args[0])
@@ -49,7 +52,8 @@ public class MacroCommandsFeature : Feature{
         ).Execute();
     }
     [When("Команды кладутся и исполняются в очереди")]
-    public void Test(){
+    public void Test()
+    {
         var order1 = new Mock<Order>();
         order1.Setup(o => o.target).Returns(test_obj.Object);
         order1.Setup(o => o.cmd).Returns("Commands.Move");
@@ -70,7 +74,7 @@ public class MacroCommandsFeature : Feature{
         ).Put(
             IoC.Resolve<ICommand>("Game.StartCommand", order1.Object)
         );
-        
+
         IoC.Resolve<IQueue<ICommand>>(
             "Game.Queue"
         ).Put(
@@ -97,21 +101,26 @@ public class MacroCommandsFeature : Feature{
     }
 
     [Then("В объекте оказываются все сгенерированные команды")]
-    public void qq(){
+    public void qq()
+    {
         Assert.IsType<MoveCommand>(test_obj.Object.properties.Get("Commands.Move"));
         Assert.IsType<SomeCommand>(test_obj.Object.properties.Get("Commands.SomeCommand"));
         Assert.IsType<AnotherCommand>(test_obj.Object.properties.Get("Commands.AnotherCommand"));
     }
 }
 
-internal class SomeCommand : ICommand{
-        public void Execute(){
+internal class SomeCommand : ICommand
+{
+    public void Execute()
+    {
 
-        }
     }
+}
 
-    internal class AnotherCommand : ICommand{
-        public void Execute(){
+internal class AnotherCommand : ICommand
+{
+    public void Execute()
+    {
 
-        }
     }
+}

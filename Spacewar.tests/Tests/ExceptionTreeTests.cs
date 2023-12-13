1e0fe0b2ce0dec1ff4f7f18c4d2496e58402f82b
test4ment <88853792+test4ment@users.noreply.github.com>
2023-12-13 16:31:05 +0000

    added exception tree test

---
+++ Spacewar.tests/Tests/ExceptionTreeTests.cs
@@ -0,0 +1,51 @@
+namespace Spacewar.Tests;
+
+public class ExceptionFeatures : Feature{
+
+    public IDictionary<object, object> ExceptionTree = new Dictionary<object, object>();
+    
+    [Fact]
+    public void Test(){
+
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Trees.Exceptions",
+            (object[] args) =>{return ExceptionTree;}
+        ).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>(
+            "IoC.Register",
+            "Trees.AddRecord",
+            (object[] args) =>
+            {
+                return new DecisionTree_AddRecord(
+                    (IDictionary<object, object>)args[0],
+                    (object[])args[1],
+                    args[2]);
+            }
+        ).Execute();
+
+        IoC.Resolve<ICommand>(
+            "Trees.AddRecord",
+            IoC.Resolve<IDictionary<object, object>>("Trees.Exceptions"),
+            new object[] {typeof(IMoveable), typeof(NotSupportedException)},
+            typeof(NotImplementedException) // new ActionCommand(() => {}) // throw new NotImplementedException();
+        ).Execute();
+
+        var manual_dict = new Dictionary<object, object>() {
+            {
+                typeof(IMoveable),
+                new Dictionary<object, object>() {
+                    {typeof(NotSupportedException),
+                    typeof(NotImplementedException) 
+                    // new ActionCommand(() => {})
+                    }
+                }
+            }
+        };
+
+        Assert.Equal(IoC.Resolve<Dictionary<object, object>>("Trees.Exceptions"), manual_dict);
+    }
+}

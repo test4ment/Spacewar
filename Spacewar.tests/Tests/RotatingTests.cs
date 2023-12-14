f4c2f68bb065946d60aa8880a7cee7bf33980a04
Zadorozhnyi <sz1253265@gmail.com>
2023-12-14 15:25:50 +0000

    1

--- Spacewar.tests/Tests/RotatingTests.cs
+++ Spacewar.tests/Tests/RotatingTests.cs
@@ -9,8 +9,8 @@ public class RotatingFeatures : Feature
     [Given(@"Для объекта, находящегося под углом к горизонту в \(-?(\d+)\) и поворачивающегося со скоростью \((-?\d+)\)")]
     public void ObjectAngleVel(int ang1, int vel1)
     {
-        rotating_object.Setup(obj => obj.angle).Returns(new int(ang1)).Verifiable();
-        rotating_object.Setup(obj => obj.angle_velocity).Returns(new int(vel1)).Verifiable();
+        rotating_object.Setup(obj => obj.angle).Returns(ang1).Verifiable();
+        rotating_object.Setup(obj => obj.angle_velocity).Returns(vel1).Verifiable();
     }
 
     [When(@"Execute")]
@@ -38,21 +38,21 @@ public class RotatingFeatures : Feature
     public void RotatingCantGetAng()
     {
         rotating_object.Setup(obj => obj.angle).Throws(new Exception());
-        rotating_object.Setup(obj => obj.angle_velocity).Returns(new int (90));
+        rotating_object.Setup(obj => obj.angle_velocity).Returns(90);
     }
 
     [Given("Объект, у которого невозможно прочитать значение угловой скорости")]
     public void RotatingCantGetVel()
     {
-        rotating_object.Setup(obj => obj.angle).Returns(new int (45));
+        rotating_object.Setup(obj => obj.angle).Returns(45);
         rotating_object.Setup(obj => obj.angle_velocity).Throws(new Exception());
     }
 
     [Given("Объект, у которого невозможно изменить угол наклона к горизонту")]
     public void RotatingCantWriteAng()
     {
-        rotating_object.Setup(obj => obj.angle).Returns(new int (45));
-        rotating_object.Setup(obj => obj.angle_velocity).Returns(new int (90));
+        rotating_object.Setup(obj => obj.angle).Returns(45);
+        rotating_object.Setup(obj => obj.angle_velocity).Returns(90);
         rotating_object.SetupSet(obj => obj.angle = It.IsAny<int>()).Throws(new Exception());
     }
 }

dc6cb05e8b24435f1b2d772d6cfb0d57bc616529
test4ment <88853792+test4ment@users.noreply.github.com>
2024-02-03 11:21:37 +0000

    create ServerThread

---
+++ Spacewar/Server.cs
@@ -0,0 +1,37 @@
+using System.Collections.Concurrent;
+using System.Threading;
+
+public class ServerThread{
+    private Thread t;
+    private BlockingCollection<ICommand> q;
+    private bool stop = false;
+    private Action behaviour;
+
+    public ServerThread(BlockingCollection<ICommand> queue){
+        q = queue;
+        behaviour = () => {
+            var c = q.Take();
+            try{
+                c.Execute();
+            }
+            catch(Exception e){
+                IoC.Resolve<ICommand>("Trees.Exceptions.Handler", c, e);
+            }
+        };
+
+        t = new Thread(() => {
+            while(!stop){
+                behaviour();
+            }
+        });
+    }
+
+    public void Stop(){
+        stop = true;
+    }
+
+    public void SetBehaviour(Action newBeh){
+        behaviour = newBeh;
+    }
+
+}
